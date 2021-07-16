17c4c02b5f92dd0fd22605b02985d645e5a93426
Farrid Kuntoro <farridguntoro@gmail.com>
2021-07-16 19:50:05 +0700

    [HEADING] : Complete Implement UI Header

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
             <Button
-                color="blue"
+                color="gray"
                 buttonType="outline"
                 rounded={true}
                 iconOnly={true}
@@ -16,8 +16,31 @@ function Header() {
             </Button>
             <Icon name="description" size="5xl" color="blue" />
             <h1 className="ml-2 text-gray-700 text-2xl">
-                Docs Google
+                Docs
             </h1>
+
+            <div className="mx-5 md:mx-20 flex flex-grow item-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
+                <Icon name="search" size="3xl" color="gray" />
+                <input type="text" placeholder="Search" className="flex-grow px-5 text-base bg-transparent outline-none" />
+            </div>
+
+            <Button
+                color="gray"
+                buttonType="outline"
+                rounded={true}
+                iconOnly={true}
+                ripple="dark"
+                className="ml-5 md:ml-20 h-20 w-20 border-0"
+            >
+                <Icon name="apps" size="3xl" color="gray" />
+            </Button>
+
+            <img
+                loading="lazy"
+                className="cursor-pointer h-12 w-12 rounded-full ml-2"
+                src="https://lh3.googleusercontent.com/ogw/ADea4I4NSewNI_jPCky1Zp3OEs9Prmob2nO3UYhxVyi_-g=s83-c-mo"
+                alt="Photo Profile"
+            />
         </div>
     );
 }
